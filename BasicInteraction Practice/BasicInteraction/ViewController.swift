92164a5f0b9142da5d099b038d0d1debce4bf27e
Eric Champion <ericmchampion@gmail.com>
2015-07-14 20:47:37 -0400

    Initial commit
    Basic practice with delegation with viewController. Added feature when
    click off text box keyboard closes, clicking ‘Done’ closes keyboard,
    and clicking ‘button’ closes keyboard.

---
+++ BasicInteraction Practice/BasicInteraction/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  BasicInteraction
+//
+//  Created by Eric Champion on 7/14/15.
+//  Copyright (c) 2015 Eric Champion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var simpleTextField: UITextField!
+    
+    @IBOutlet weak var simpleLabel: UILabel!
+    
+    @IBAction func changeLabel(sender: AnyObject) {
+        simpleLabel.text = "Hello " + simpleTextField.text + "!"
+        self.simpleTextField.resignFirstResponder()
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
